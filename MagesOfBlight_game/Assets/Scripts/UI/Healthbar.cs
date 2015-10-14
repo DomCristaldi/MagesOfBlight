cd9e95d8b81aec6154fc047211ab4aefe71e0fca
Bparker10 <parker4@rpi.edu>
2015-10-13 22:53:25 -0400

    health bar is round and works as a placeholder

---
+++ MagesOfBlight_game/Assets/Scripts/UI/Healthbar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Healthbar : MonoBehaviour {
+
+	public Image circularSilder;            //Healthbar image
+	public float time;                      //In how much time the progress bar will fill/empty
+	void Start() {
+		circularSilder.fillAmount=0f;      // Initally progress bar is empty
+	}
+	void Update () {
+		circularSilder.fillAmount += Time.deltaTime / time;         
+	}
+}
