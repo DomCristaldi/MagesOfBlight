7c05c9d848d80e57b037d4d2dd310071bed26f45
Bparker10 <parker4@rpi.edu>
2015-10-15 15:08:24 -0400

    messing around with buttons, found out how to change all the colors, still need a good way of hooking each button with the correct move

--- MagesOfBlight_game/Assets/Scripts/UI/UIHandler.cs
+++ MagesOfBlight_game/Assets/Scripts/UI/UIHandler.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class UIHandler : MonoBehaviour {
 
@@ -10,15 +11,34 @@ public class UIHandler : MonoBehaviour {
 	//reference to each characters' canvas
 	public Canvas avonCanvas;
 
+	//list of commands
+	public List<string> avonCommands = new List<string>();
+
 	// Use this for initialization
 	void Start () {
-		GameObject tmp = Instantiate (actionButton.gameObject) as GameObject;
-		tmp.transform.SetParent (avonCanvas.transform);
 
+		CreateButtons ("Avon", avonCommands);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	//take in list of moves and turn them into buttons
+	void CreateButtons(string character, List<string> commands){
+		foreach(string command in commands){
+			//create button
+			GameObject tmp = (GameObject)Instantiate (actionButton.gameObject);
+			//set parent to apporopriate character canvas
+			tmp.transform.SetParent (avonCanvas.transform);
+			//change text of button
+			tmp.GetComponentInChildren<Text>().text = command;
+			//change text color of button
+
+			//change background color
+			ColorBlock buttonColors = tmp.GetComponent<Button>().colors;
+
+		}
 	}
 }
