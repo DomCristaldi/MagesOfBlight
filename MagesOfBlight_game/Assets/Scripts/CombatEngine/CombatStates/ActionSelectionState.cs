a498acc2557977b02e6cb7cc960a7b61cebd775d
DomCristaldi <domeniccristaldi@gmail.com>
2015-10-15 19:14:58 -0400

    State machine is set up in a very basic mode right now You can get a path by clicking on one hex and then clicking on another hex

---
+++ MagesOfBlight_game/Assets/Scripts/CombatEngine/CombatStates/ActionSelectionState.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionSelectionState : BaseCombatState {
+
+    public override void UpdateState() {
+        base.UpdateState();
+
+        Debug.Log("Select action");
+
+        battleManRef.ChangeCombatState(BattleManager.CombatPhase.TargetSelection);
+    }
+
+}
