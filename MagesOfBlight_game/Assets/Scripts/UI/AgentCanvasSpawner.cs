d38e1c29a492fd1b9f80d831764fa9de46bd2e96
Bparker10 <parker4@rpi.edu>
2015-10-19 18:39:55 -0400

    moves canvas spawner to scripts -> UI. canvas spawner spawns canvas with buttons based on action list gotten from agent and changes the text and all the colors of button created. Script needs button and canvas prefab located in prefabs -> UI assigned in the inspector

---
+++ MagesOfBlight_game/Assets/Scripts/UI/AgentCanvasSpawner.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(AgentActions))]
+public class AgentCanvasSpawner : MonoBehaviour {
+
+	//assigned on start
+	public AgentActions agentActions;
+
+	//need button prefab assigned in inspector
+	public Button actionButtonPrefab;
+
+	//list of possible actions
+	private List<AgentActions.ActionData> actionDataList;
+
+	//list of action buttons
+	private List<GameObject> actionButtonList;
+
+	//canvas on the agent, need prefab assigned in inspector
+	public Canvas agentCanvasPrefab;
+
+	void Start () {
+		//assign agent actions var
+		agentActions = GetComponent<AgentActions>();
+		actionDataList = agentActions.actionDataList;
+
+		//create ui for the agent
+		CreateUI ();
+	}
+
+	void CreateUI(){
+		//create canvas and parent it to the agent
+		GameObject agentCanvas = (GameObject)Instantiate (agentCanvasPrefab.gameObject);
+		agentCanvas.transform.SetParent (this.transform);
+		//create buttons
+		foreach (AgentActions.ActionData actionData in actionDataList) {
+			//create button
+			GameObject tmp = (GameObject)Instantiate (actionButtonPrefab.gameObject);
+			//set parent to canvas
+			tmp.transform.SetParent (agentCanvasPrefab.transform);
+			//change text of button
+			tmp.GetComponentInChildren<Text>().text = actionData.action.actionName;
+			//change button colors
+			ColorBlock buttonColors = tmp.GetComponent<Button>().colors;
+			buttonColors.normalColor = actionData.action.normalColor;
+			buttonColors.highlightedColor = actionData.action.highlightedColor;
+			buttonColors.pressedColor = actionData.action.pressedColor;
+			buttonColors.disabledColor = actionData.action.disabledColor;
+			//change button text color
+			if(buttonColors.normalColor != Color.white){
+				//if its not white, the text should be white
+				tmp.GetComponentInChildren<Text>().color = Color.white;
+			}
+			else{
+				//else text should be black
+				tmp.GetComponentInChildren<Text>().color = Color.black;
+			}
+			//add button to list
+			actionButtonList.Add(tmp);
+		}
+
+	}
+	
+}
