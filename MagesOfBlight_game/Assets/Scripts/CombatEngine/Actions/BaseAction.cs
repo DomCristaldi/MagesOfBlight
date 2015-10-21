4e969665b094159add619d0c7af2195f657c7942
Justin Cirigliano <cirigj@rpi.edu>
2015-10-20 21:30:23 -0400

    ++ Added EnumHelper to JBirdEngine ++ Started sub-classing Actions

--- MagesOfBlight_game/Assets/Scripts/CombatEngine/Actions/BaseAction.cs
+++ MagesOfBlight_game/Assets/Scripts/CombatEngine/Actions/BaseAction.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using JBirdEngine;
 
 public class BaseAction : ScriptableObject {
 	
@@ -9,6 +10,16 @@ public class BaseAction : ScriptableObject {
 	public Color pressedColor;
 	public Color disabledColor;
 	public string actionName;
+
+	float range = Mathf.Infinity;
+
+	public enum TileCheckFlags {
+		none = 0,
+		unoccupied = 1,
+		occupied = 2,
+	}
+
+	public bool passThroughOccupied = false;
 
 	public virtual List<HexNode> CheckTiles () {
 		return new List<HexNode>();
