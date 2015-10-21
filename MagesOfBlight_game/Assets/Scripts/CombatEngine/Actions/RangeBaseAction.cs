f35ba80ee22cbaf072cec970b3e7cc7d43744d3c
Justin Cirigliano <cirigj@rpi.edu>
2015-10-20 23:00:35 -0400

    - - Removed .node from TileInfo . . TileInfo now inherits from HexNode

--- MagesOfBlight_game/Assets/Scripts/CombatEngine/Actions/RangeBaseAction.cs
+++ MagesOfBlight_game/Assets/Scripts/CombatEngine/Actions/RangeBaseAction.cs
@@ -6,10 +6,39 @@ public class RangeBaseAction : BaseAction {
 
 	public override List<HexNode> CheckTiles () {
 		List<HexNode> nodeList = new List<HexNode>();
-		HexNode start = BattleManager.singleton.selectedTile.node;
-		int dist = 1;
-		for (int i = 1; i < checkDistance; i++) {
-
+		HexNode start = BattleManager.singleton.selectedTile;
+		List<HexNode> outerRing = new List<HexNode>();
+		List<HexNode> innerCircle = new List<HexNode>();
+		List<HexNode> nextRing = new List<HexNode>();
+		outerRing.Add(start);
+		int dist;
+		for (dist = 0; dist < checkDistance; dist++) {
+			if (outerRing.Count == 0) {
+				break;
+			}
+			foreach (HexNode ringNode in outerRing) {
+				foreach (HexNode node in ringNode.connections) {
+					if (node == null) {
+						continue;
+					}
+					if (outerRing.Contains(node)) {
+						continue;
+					}
+					if (innerCircle.Contains(node)) {
+						continue;
+					}
+					if (nextRing.Contains(node)) {
+						continue;
+					}
+					
+				}
+			}
+			while (outerRing.Count > 0) {
+				innerCircle.Add(outerRing[0]);
+				outerRing.RemoveAt(0);
+			}
+			outerRing = new List<HexNode>(nextRing);
+			nextRing.Clear();
 		}
 		return nodeList;
 	}
