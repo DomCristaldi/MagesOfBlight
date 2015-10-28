a09fbd17862a030b4873f09480f4366f1598f408
Justin Cirigliano <cirigj@rpi.edu>
2015-10-28 17:54:48 -0400

    . . finalized projectile action

--- MagesOfBlight_game/Assets/Scripts/CombatEngine/Actions/BaseProjectile.cs
+++ MagesOfBlight_game/Assets/Scripts/CombatEngine/Actions/BaseProjectile.cs
@@ -6,6 +6,7 @@ public class BaseProjectile : MonoBehaviour {
 	public Transform target;
 	public float speed;
 	public float damage;
+	public ProjectileAction action;
 
 	public bool CheckProximity () {
 		return (Vector3.Distance(target.position, transform.position) < speedPerStep);
@@ -13,6 +14,10 @@ public class BaseProjectile : MonoBehaviour {
 
 	float speedPerStep {
 		get { return speed * Time.deltaTime; }
+	}
+
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, target.position, speedPerStep);
 	}
 
 	public virtual void DealDamage () {
@@ -23,6 +28,7 @@ public class BaseProjectile : MonoBehaviour {
 	}
 
 	public virtual void DestroySelf () {
+		action.projectile = null;
 		Destroy(gameObject);
 	}
 
