71c2bc43c6ce56794c7a94ed33b0176bf01c3ce8
Justin Cirigliano <cirigj@rpi.edu>
2015-11-04 17:39:45 -0500

    ++ Added Scripts->UI->Dialogue folder ++ Added CharacterPortraitLibrary and Conversation ScriptableObject ++ Added CharacterPortraitLibrary to MasterController prefab ++ Added a TestConvo asset ++ Conversations can now be instantiated from the Create Asset Menu

---
+++ MagesOfBlight_game/Assets/Scripts/UI/Dialogue/Conversation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum Character {
+	Avon,
+	Nuria,
+	Stein,
+}
+
+public enum Mood {
+	Neutral,
+	Angry,
+}
+
+[System.Serializable]
+public class DialogueObject {
+
+	public Character character;
+	public Mood mood;
+	public string line;
+	public AudioClip audioClip;
+
+	public Sprite sprite {
+		get { return GetSprite(); }
+	}
+
+	private Sprite GetSprite () {
+		foreach (CharacterMoodSprites playerCharacter in CharacterPortraitLibrary.singleton.characters) {
+			if (playerCharacter.character == character) {
+				foreach (MoodSprite moodSprite in playerCharacter.moodSprites) {
+					if (moodSprite.mood == mood) {
+						return moodSprite.sprite;
+					}
+				}
+				return playerCharacter.defaultPortrait;
+			}
+		}
+		return CharacterPortraitLibrary.singleton.defaultPortrait;
+	}
+
+}
+
+[CreateAssetMenuAttribute]
+public class Conversation : ScriptableObject {
+	
+	public List<DialogueObject> dialogue;
+
+}
