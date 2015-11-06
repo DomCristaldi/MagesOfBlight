4e3e6cb969d020e1cac8bd8d6c9b4208a10338c3
Bparker10 <parker4@rpi.edu>
2015-11-06 00:23:42 -0500

    conversation handler can now handle any number of conversations, turning off dialogue box inbetween

--- MagesOfBlight_game/Assets/Scripts/UI/Dialogue/UIConversationHandler.cs
+++ MagesOfBlight_game/Assets/Scripts/UI/Dialogue/UIConversationHandler.cs
@@ -4,38 +4,58 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 
 public class UIConversationHandler : MonoBehaviour {
-	//conversation object created from Assests -> create
-	public Conversation convo;
+	//list of conversation objects created from Assests -> create
+	public List<Conversation> convo = new List<Conversation>();
+
+	public Canvas dialogueCanvas;
 
 	//dialogue tings that need to change
 	public Text textDialogue;
 	public Image characterImage;
 	//audioclip to be played
 	public AudioClip audioClip;
-	//number for handling the conversation list
-	public int indexNumber;
+	//number for handling the lists
+	int dialogueIndexNumber;
+	int conversationIndexNumber;
 
 	//list of dialogue obj from the conversation
 	List<DialogueObject> dialogueList = new List<DialogueObject>();
 
+	void Awake(){
+		DontDestroyOnLoad (gameObject);
+	}
+
 	// Use this for initialization
 	void Start () {
-		//init dialogue list
-		dialogueList = convo.dialogue;
+		//init
+		conversationIndexNumber = 0;
+		dialogueIndexNumber = 0;
+		dialogueList = convo[0].dialogue;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//if enter is pressed, move to next convo
-		if(Input.GetKeyDown(KeyCode.Return)){
-			indexNumber++;
-			showConversation(indexNumber);
+		//--------below is for testing only---------
+		if (Input.GetKeyDown (KeyCode.Return) && !dialogueCanvas.enabled) {
+			dialogueCanvas.enabled = true;
+		}
+		//if enter is pressed, move to next convo as long as the dialogue is not over yet
+		if(Input.GetKeyDown(KeyCode.Return) && dialogueIndexNumber < dialogueList.Count-1){
+			showConversation(dialogueIndexNumber);
+			dialogueIndexNumber++;
+		}
+		//if enter is pressed and the conversation is done, turn off dialogue box and interate conversation number
+		if (Input.GetKeyDown (KeyCode.Return) && dialogueIndexNumber == dialogueList.Count - 1) {
+			dialogueCanvas.enabled = false;
+			dialogueIndexNumber = 0;
+			conversationIndexNumber++;
+			if(conversationIndexNumber < convo.Count) dialogueList = convo[conversationIndexNumber].dialogue;
+			else Destroy(gameObject);
 		}
 	}
 
 	//function to be called each iteration of the list
 	void showConversation(int indexInList){
-		if (indexInList > dialogueList.Count) return;
 		textDialogue.text = dialogueList [indexInList].line;
 		characterImage.sprite = dialogueList [indexInList].sprite;
 	}
