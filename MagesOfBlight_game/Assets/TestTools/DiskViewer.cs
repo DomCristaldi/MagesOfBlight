d6c233243417a24d9e2a5f887f9895dae9ad511b
unknown <CRISTD@SA2502-PC01.win.rpi.edu>
2015-11-05 21:36:45 -0500

    .. Repositioned camera in Characters scene ?? was trying to make an editor tool to assist positioning models, hitting a roadblock, gonna do it later

---
+++ MagesOfBlight_game/Assets/TestTools/DiskViewer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiskViewer : MonoBehaviour {
+
+    public Color discColor = Color.blue;
+
+    public float discRadius = 0.5f;
+
+    public Mesh assetMesh;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
