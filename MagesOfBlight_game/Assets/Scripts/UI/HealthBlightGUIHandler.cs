6f5c88e5380aae7da0e073404e8ba2787c0ce8cc
Bparker10 <parker4@rpi.edu>
2015-11-05 22:17:19 -0500

    gui health and blight hooked up

---
+++ MagesOfBlight_game/Assets/Scripts/UI/HealthBlightGUIHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthBlightGUIHandler : MonoBehaviour {
+
+	public Image healthMeter;
+	public Image blightMeter;
+
+	public BaseStats stats;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		healthMeter.fillAmount = stats.currentHealth/200f;
+		blightMeter.fillAmount = stats.currentBlight/200f;
+	}
+}
