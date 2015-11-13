56cfdc73e40b18e3ef9cb5bfd758e9e7a3c4744d
unknown <cristd@SA2411-PC42.win.rpi.edu>
2015-11-13 01:24:13 -0500

    starting integration of dialouge

---
+++ MagesOfBlight_game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager _singleton;
+
+    public static GameManager singleton {
+        get { return _singleton; }
+    }
+
+    public BattleManager battleMan;
+
+    void Awake() {
+        if (_singleton == null) {
+            _singleton = this;
+        }
+        else { Destroy(this); }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
