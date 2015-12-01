f4a5feae55db44d05f9c1a8b4e88caadb3eb5e18
unknown <cirigj@SA2411-PC42.win.rpi.edu>
2015-11-30 23:12:29 -0500

    . . More AI stuff

--- MagesOfBlight_game/Assets/Scripts/CombatEngine/CombatStates/AILogic.cs
+++ MagesOfBlight_game/Assets/Scripts/CombatEngine/CombatStates/AILogic.cs
@@ -4,6 +4,13 @@ using System.Collections.Generic;
 
 public class AILogic : BaseCombatState {
 
+    public enum BehaviourProtocol {
+        basic,//rush above 50% health, range below 50% health
+        rush,//get close and melee
+        range,//stay 2 tiles away and use magic
+        puppeteer,//behaviour used by the hivemind
+    }
+
     public AILogic(BattleManager.CombatPhase thisCombatPhase, bool canUndo = false, bool canHoverGrid = false) : base(thisCombatPhase, canUndo, canHoverGrid) { }
 
     public override void InitState () {
@@ -31,33 +38,84 @@ public class AILogic : BaseCombatState {
         HexNode targetTile = aggroTarget.motor.currentTile;
         HexNode currentTile = currentAI.motor.currentTile;
 
-        //Try to move closer to target first
-        if (currentAI.moveSteps > 0) {
-            if (!currentTile.connections.Contains(targetTile)) {
-                foreach (AgentActions.ActionData actionData in currentAI.agentActions.proactiveActions) {
-                    if (actionData.action as MoveAction != null) {
-                        battleManRef.selectedAction = actionData.action;
-                        break;
-                    }
-                }
-                if (battleManRef.selectedAction == null) {
-                    Debug.LogError("AILogic: Enemy {0} has no move action!");
-                    currentAI.moveSteps = 0;
-                }
-                else {
-                    battleManRef.targetTile = JBirdEngine.ListHelper.GetClosestToPosition<HexNode>(targetTile.connections, currentAI.transform.position);
-                    battleManRef.ChangeCombatState(BattleManager.CombatPhase.PerformAction);
-                }
+        //BEHAVIOUR STUFF (maybe change to Strategy pattern later)
+
+        //Determine behaviour protocol
+        BehaviourProtocol currentProtocol = currentAI.behaviourProtocol;
+        if (currentProtocol == BehaviourProtocol.basic) {
+            if (currentAI.stats.currentHealth >= currentAI.stats.maxHealth / 2f) {
+                currentProtocol = BehaviourProtocol.rush;
+            }
+            else {
+                currentProtocol = BehaviourProtocol.range;
             }
         }
 
-        //Pick the best attack or end the turn if none are available
-        List<BaseAction> potentialActions = new List<BaseAction>();
-        foreach (AgentActions.ActionData actionData in currentAI.agentActions.proactiveActions) {
-            if (actionData.action as MoveAction != null || actionData.action as DoNothingAction != null) {
-                continue;
+        //RUSH
+        if (currentProtocol == BehaviourProtocol.rush) {
+
+            //Try to move closer to target first
+            if (currentAI.moveSteps > 0) {
+                if (!currentTile.connections.Contains(targetTile)) {
+                    foreach (AgentActions.ActionData actionData in currentAI.agentActions.proactiveActions) {
+                        if (actionData.action as MoveAction != null) {
+                            battleManRef.selectedAction = actionData.action;
+                            break;
+                        }
+                    }
+                    if (battleManRef.selectedAction == null) {
+                        Debug.LogError("AILogic: Enemy {0} has no move action!");
+                        currentAI.moveSteps = 0;
+                    }
+                    else {
+                        battleManRef.targetTile = JBirdEngine.ListHelper.GetClosestToPosition<HexNode>(targetTile.connections, currentAI.transform.position);
+                        battleManRef.ChangeCombatState(BattleManager.CombatPhase.PerformAction);
+                        return;
+                    }
+                }
             }
 
+            //Do melee if within range
+            if (currentTile.connections.Contains(targetTile)) {
+                foreach (AgentActions.ActionData actionData in currentAI.agentActions.proactiveActions) {
+                    if (actionData.action as BasicMeleeAction != null) {
+                        battleManRef.selectedAction = actionData.action;
+                        battleManRef.targetTile = targetTile;
+                        battleManRef.ChangeCombatState(BattleManager.CombatPhase.PerformAction);
+                        return;
+                    }
+                }
+            }
+
+            //Pick the best attack or end the turn if none are available
+            List<BaseAction> potentialActions = new List<BaseAction>();
+            foreach (AgentActions.ActionData actionData in currentAI.agentActions.proactiveActions) {
+                if (actionData == null || actionData.action == null) {
+                    Debug.LogErrorFormat("AILogic: Enemy {0} has a null action!");
+                    continue;
+                }
+                if (actionData.action as MoveAction != null) {
+                    continue;
+                }
+                if (actionData.action as DoNothingAction != null) {
+                    continue;
+                }
+                if (actionData.action as BasicMeleeAction != null) {
+                    continue;
+                }
+                if (actionData.action.CheckTiles().Contains(targetTile)) {
+                    potentialActions.Add(actionData.action);
+                }
+            }
+            if (potentialActions.Count > 0) {
+                //Select the action with the highest damage
+
+            }
+            else {
+                battleManRef.selectedAction = battleManRef.defaultAction;
+                battleManRef.targetTile = targetTile;
+            }
+            battleManRef.ChangeCombatState(BattleManager.CombatPhase.PerformAction);
         }
 
     }
