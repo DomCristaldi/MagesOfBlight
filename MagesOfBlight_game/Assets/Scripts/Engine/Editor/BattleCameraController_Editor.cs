59ad383c07c4359a3921685d2adc4721c4bcc719
unknown <cristd@SA2411-PC41.win.rpi.edu>
2015-12-02 01:07:02 -0500

    .. added visualization for the y axis

--- MagesOfBlight_game/Assets/Scripts/Engine/Editor/BattleCameraController_Editor.cs
+++ MagesOfBlight_game/Assets/Scripts/Engine/Editor/BattleCameraController_Editor.cs
@@ -31,6 +31,11 @@ public class BattleCameraController_Editor : Editor {
         Handles.DrawLine(new Vector3(controller.clampReferencePoint.position.x + controller.clampDistX, controller.clampReferencePoint.position.y, controller.clampReferencePoint.position.z - controller.clampDistZ),
                          new Vector3(controller.clampReferencePoint.position.x + controller.clampDistX, controller.clampReferencePoint.position.y, controller.clampReferencePoint.position.z + controller.clampDistZ));
 
+
+        Handles.DrawLine(new Vector3(controller.transform.position.x, controller.clampReferencePoint.position.y - controller.clampDistY, controller.transform.position.z),
+                         new Vector3(controller.transform.position.x, controller.clampReferencePoint.position.y + controller.clampDistY, controller.transform.position.z));
+
+
         Handles.color = originalColor;
     }
 
