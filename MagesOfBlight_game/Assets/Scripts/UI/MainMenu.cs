0ef606b1f3f695b50c3fcb41ab895bc909e276b0
Bparker10 <parker4@rpi.edu>
2015-12-02 13:30:45 -0500

    main menu skeleton

---
+++ MagesOfBlight_game/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public void Play(){
+		Application.LoadLevel (1);
+	}
+
+	public void About(){
+		
+	}
+
+	public void Quit(){
+		Application.Quit ();
+	}
+}
