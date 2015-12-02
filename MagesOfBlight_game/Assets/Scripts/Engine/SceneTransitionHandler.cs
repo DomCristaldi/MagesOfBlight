104e20188980b36dd7732c9fcca070442e446650
unknown <CRISTD@SA2411-PC15.win.rpi.edu>
2015-12-02 15:17:35 -0500

    Level Transition Framework is in

---
+++ MagesOfBlight_game/Assets/Scripts/Engine/SceneTransitionHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class SceneTransitionHandler : MonoBehaviour {
+
+    private static SceneTransitionHandler singleton;
+
+    void Awake() {
+        if (singleton == null) { singleton = this; }
+        else { Destroy(this.gameObject); }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
