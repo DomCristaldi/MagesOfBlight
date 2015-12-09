4b3a36c4fa3fbab081fc2cc572ba933b71c4ac55
unknown <CRISTD@SA2411-PC15.win.rpi.edu>
2015-12-09 07:02:09 -0500

    Looping around back to the beginning of the game now works

---
+++ MagesOfBlight_game/Assets/Scripts/UI/ConnectButtonToGameController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ConnectButtonToGameController : MonoBehaviour {
+
+    Button butt;
+
+    void Awake() {
+        butt = GetComponent<Button>();
+    }
+
+	// Use this for initialization
+	void Start () {
+        butt.onClick.AddListener(() => { SceneTransitionHandler.singleton.LoadNextLevel(); });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
